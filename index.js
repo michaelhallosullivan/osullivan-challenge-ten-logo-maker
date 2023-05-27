d9db7307bd12a0b37fc76233a20dc04eac024088
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-05-27 16:45:07 -0700

    logs confirmation of logo.svg to console

--- index.js
+++ index.js
@@ -43,14 +43,14 @@ inquirer
             Shape = Triangle
     };
     let logoSVG =
-        `<svg width="300px" height="200px" xmlns="http://www.w3.org/2000/svg">
-            <g>
-                <${Shape} fill="${generatedLogo.shapeColor}"/>
-                <text font="Georgia" fill="${generatedLogo.textColor}">
-                ${generatedLogo.text}
-                </text>
-            </g>
-        </svg>`;
+    `<svg width="300px" height="200px" xmlns="http://www.w3.org/2000/svg">
+        <g>
+            <${Shape} fill="${generatedLogo.shapeColor}"/>
+            <text font="Georgia" fill="${generatedLogo.textColor}">
+            ${generatedLogo.text}
+            </text>
+        </g>
+    </svg>`;
     fs.writeFile("logo.svg", logoSVG, (err) =>
     err ? console.error(err) : console.log("Generated logo.svg")
     );
