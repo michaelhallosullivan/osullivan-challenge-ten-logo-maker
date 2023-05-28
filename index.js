e8330731e5a6efdd9176193520e0dfb6b762630d
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-05-27 20:25:04 -0700

    started jest validation templates, turned shape constants into classes to fulfill readme requirements

--- index.js
+++ index.js
@@ -34,18 +34,20 @@ inquirer
     const generatedLogo = new Logo(response.shape, response.shapeColor, response.text, response.textColor);
     let Shape = "";
     if (generatedLogo.shape === "circle") {
-            Shape = Circle;
+            Shape = new Circle();
         }
     else if (generatedLogo.shape === "square") {
-            Shape = Square;
+            Shape = new Square();
         }
     else if (generatedLogo.shape === "triangle") {
-            Shape = Triangle;
+            Shape = new Triangle();
     };
     let logoSVG =
     `<svg width="300px" height="200px" xmlns="http://www.w3.org/2000/svg">
-        <${Shape} fill="${generatedLogo.shapeColor}"/>
-        <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="30px" font-family="Georgia" fill="${generatedLogo.textColor}">${generatedLogo.text}</text>
+        <g>
+        <${Shape.value} fill="${generatedLogo.shapeColor}"/>
+        <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="40px" font-family="Georgia" fill="${generatedLogo.textColor}">${generatedLogo.text}</text>
+        </g>
     </svg>`;
     fs.writeFile("logo.svg", logoSVG, (err) =>
     err ? console.error(err) : console.log("Generated logo.svg")
